f27b146d1e1a20213eacae0f3712b08b2d6dcca8
Dado <moslyi0891@gmail.com>
2020-01-30 14:33:31 +0100

    Dado-2

--- src/todoliste/MainToDoListe.java
+++ src/todoliste/MainToDoListe.java
@@ -5,14 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import todoliste.view.BearbeiteEintragToDoListeController;
 
 public class MainToDoListe extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/HauptfensterToDoListe.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/BearbeiteEintragToDoListe.fxml"));
         primaryStage.setTitle("ToDo Liste");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 570, 300));
         primaryStage.show();
     }
 
