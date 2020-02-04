41b32fe5ac9d85eb4406bb17909d0990f1a0488f
dopeBlaze <m.broja@web.de>
2020-02-04 23:17:33 +0100

    Aus Branch MarkusNeuerEintrag

--- src/todoliste/view/NeuerEintragToDoListeController.java
+++ src/todoliste/view/NeuerEintragToDoListeController.java
@@ -1,22 +1,17 @@
 package todoliste.view;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import todoliste.datenbank.beans.AktivitaetsEintragBean;
+import todoliste.model.AktivitaetsEintrag;
+import java.time.LocalDate;
 
 public class NeuerEintragToDoListeController {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
 
     @FXML
     private Button btnEintragHinzufuegen;
@@ -25,13 +20,13 @@ public class NeuerEintragToDoListeController {
     private TextField tfNeuerEintrag;
 
     @FXML
-    private TableView<?> TVAktivitaetsname;
+    private TableView<AktivitaetsEintrag> TVAktivitaetsname;
 
     @FXML
-    private MenuButton btnLabel;
+    private ObservableList<AktivitaetsEintrag> tableData;
 
     @FXML
-    private MenuButton btnPrioritaet;
+    private FilteredList<AktivitaetsEintrag> tableFilteredData;
 
     @FXML
     private DatePicker btnCalPickStart;
@@ -40,28 +35,60 @@ public class NeuerEintragToDoListeController {
     private DatePicker btnCalPickEnd;
 
     @FXML
-    void setzeEnddatum(ActionEvent event) {
-
-    }
+    private ChoiceBox<String> cbKategorie;
 
     @FXML
-    void setzeLabel(ActionEvent event) {
+    private ChoiceBox<String> cbPrioritaet;
 
+
+    /**
+     * Neuer Eintrag wird mit den gesetzten Werten hinzugefügt
+     */
+    @FXML
+    void addNeuenEintrag() {
+
+        try{
+            AktivitaetsEintrag selected = TVAktivitaetsname.getSelectionModel().getSelectedItem();
+            AktivitaetsEintrag neuerEintrag = new AktivitaetsEintrag(selected.getAktivitaetsName(), btnCalPickStart.getValue().toString(), btnCalPickEnd.getValue().toString(), 0, cbKategorie.getValue(), cbPrioritaet.getValue(), "nicht gestartet");
+            AktivitaetsEintragBean.saveAktivitaet(neuerEintrag);
+            // Rückmeldung wenn erfolgreich
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Erfolgreich!");
+            alert.setContentText("Aktivität wurde hinzugefügt.");
+            alert.showAndWait();
+
+        } catch (NullPointerException e){
+            // Rückmeldung wenn Fehler
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Hinzufügen nicht möglich!");
+            alert.setContentText("Aktivitaetsname fehlt!\nBitte einen Namen auswählen!");
+            alert.showAndWait();
+        }
     }
 
-    @FXML
-    void setzeNeuenEintrag(ActionEvent event) {
 
+    /**
+     * Datumsabgleich der DatePicker
+     * Wenn Startdatum spaeter als Enddatum, dann wird Enddatum gleich Startdatum gesetzt
+     */
+    @FXML
+    void setzeStartdatum() {
+        btnCalPickStart.setShowWeekNumbers(false);
+        if (btnCalPickStart.getValue().compareTo(btnCalPickEnd.getValue()) > 0){
+            btnCalPickEnd.setValue(btnCalPickStart.getValue());
+        }
     }
 
+    /**
+     * Datumsabgleich der DatePicker
+     * Wenn Enddatum frueher als Startdatum, dann wird Startdatum gleich Enddatum gesetzt
+     */
     @FXML
-    void setzePrioritaet(ActionEvent event) {
-
-    }
-
-    @FXML
-    void setzeStartdatum(ActionEvent event) {
-
+    void setzeEnddatum() {
+        btnCalPickEnd.setShowWeekNumbers(false);
+        if (btnCalPickEnd.getValue().compareTo(btnCalPickStart.getValue()) < 0) {
+            btnCalPickStart.setValue(btnCalPickEnd.getValue());
+        }
     }
 
     @FXML
@@ -69,10 +96,60 @@ public class NeuerEintragToDoListeController {
         assert btnEintragHinzufuegen != null : "fx:id=\"btnEintragHinzufuegen\" was not injected: check your FXML file 'NeuerEintragToDoListe.fxml'.";
         assert tfNeuerEintrag != null : "fx:id=\"tfNeuerEintrag\" was not injected: check your FXML file 'NeuerEintragToDoListe.fxml'.";
         assert TVAktivitaetsname != null : "fx:id=\"TVAktivitaetsname\" was not injected: check your FXML file 'NeuerEintragToDoListe.fxml'.";
-        assert btnLabel != null : "fx:id=\"btnLabel\" was not injected: check your FXML file 'NeuerEintragToDoListe.fxml'.";
-        assert btnPrioritaet != null : "fx:id=\"btnPrioritaet\" was not injected: check your FXML file 'NeuerEintragToDoListe.fxml'.";
         assert btnCalPickStart != null : "fx:id=\"btnCalPickStart\" was not injected: check your FXML file 'NeuerEintragToDoListe.fxml'.";
         assert btnCalPickEnd != null : "fx:id=\"btnCalPickEnd\" was not injected: check your FXML file 'NeuerEintragToDoListe.fxml'.";
+        assert cbKategorie != null : "fx:id=\"cbKategorie\" was not injected: check your FXML file 'NeuerEintragToDoListe.fxml'.";
+        assert cbPrioritaet != null : "fx:id=\"cbPrioritaet\" was not injected: check your FXML file 'NeuerEintragToDoListe.fxml'.";
 
+        btnCalPickStart.setValue(LocalDate.now());
+        btnCalPickStart.getEditor().setDisable(true);
+        btnCalPickEnd.setValue(LocalDate.now());
+        btnCalPickEnd.getEditor().setDisable(true);
+
+        initTable();
+    }
+
+    /**
+     * Initialisierung der TableView
+     */
+    private void initTable() {
+        tableData = FXCollections.observableArrayList(AktivitaetsEintragBean.getAktivitaetsNamen());
+
+        // Spalten erstellen
+        TableColumn<AktivitaetsEintrag, String> tcAktivitaetsName = new TableColumn<>("Aktivitaetsname");
+        tcAktivitaetsName.setPrefWidth(334.0);
+
+        // Zuordnung Werte <-> Model
+        tcAktivitaetsName.setCellValueFactory(new PropertyValueFactory<>("aktivitaetsName"));
+
+        // Spalten hinzufügen
+        TVAktivitaetsname.getColumns().add(tcAktivitaetsName);
+
+        // Daten zuweisen
+        TVAktivitaetsname.setItems(tableData);
+
+        // Für eine gefilterte und sortierte Ansicht
+        tableFilteredData = new FilteredList<>(tableData, p -> true);
+        SortedList<AktivitaetsEintrag> tableSortedData = new SortedList<>(tableFilteredData);
+        tableSortedData.comparatorProperty().bind(TVAktivitaetsname.comparatorProperty());
+        TVAktivitaetsname.setItems(tableSortedData);
+
+        // Filter Predicate setzen
+        tfNeuerEintrag.textProperty().addListener((observable, oldValue, newValue) -> {
+            tableFilteredData.setPredicate(aktivitaetsName -> {
+                // wenn Filter leer dann zeige alle Aktivitaetsnamen
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+
+                // vergleiche Aktivitaetsnamen mit Filtertext
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if (aktivitaetsName.getAktivitaetsName().toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // Filter hat Treffer
+                }
+                return false; // Vergleich findet keine Uebereinstimmung
+            });
+        });
     }
 }
